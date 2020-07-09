85393e6dd8e6c5e05e833b425dd0998d6adabdce
Palanx <tmunizq@gmail.com>
2020-07-08 23:50:17 -0400

    ui structure done

---
+++ Assets/Scripts/SyncScrollsRect.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Die4Games
+{
+    public class SyncScrollsRect : MonoBehaviour
+    {
+        public ScrollRect scrollToSync;
+
+        public void Sync(Vector2 position)
+        {
+            scrollToSync.horizontalNormalizedPosition = position.x;
+        }
+    }
+}
